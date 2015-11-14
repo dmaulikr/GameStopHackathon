bcb1f065e64817a8b2f93704ba759a624e773f35
Manav Singh <manav2119@gmail.com>
2015-11-14 00:12:58 -0800

    Game Sharing iOS App Begins

---
+++ Team22/Team22/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Team22
+//
+//  Created by Manav Pavitra Singh on 11/14/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Team22/Team22/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Team22
+//
+//  Created by Manav Pavitra Singh on 11/14/15.
+//  Copyright (c) 2015 SJSU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
